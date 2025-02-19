d58fd53d546814e9432392f26dfd3c29e6c8098d
Robert Rees <shudderfix@gmail.com>
2025-02-19 16:43:24 +0000

    Creates a migration script describing a Link

---
+++ db/migrations/20250219163229_Link.sql
@@ -0,0 +1,33 @@
+--migrate:up
+
+CREATE OR REPLACE FUNCTION update_modified_column() 
+	RETURNS TRIGGER AS $$
+	BEGIN
+	    NEW.modified = now();
+	    RETURN NEW; 
+	END;
+	$$ language 'plpgsql';
+
+
+CREATE TABLE IF NOT EXISTS links (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	external_id uuid UNIQUE NOT NULL DEFAULT gen_random_uuid (),
+	url text NOT NULL,
+	notes text NOT NULL,
+	tags text[],
+	created timestamp default current_timestamp,
+	updated timestamp default current_timestamp
+);
+
+CREATE TRIGGER update_links_modtime
+BEFORE UPDATE ON links
+FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+
+-- migrate:down
+
+DROP TRIGGER IF EXISTS update_links_modtime ON links;
+
+DROP TABLE IF EXISTS links;
+
+DROP FUNCTION update_modified_column;
