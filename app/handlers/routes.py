42e70692edcb570abc3245c5a05c5094b961ea94
Robert Rees <shudderfix@gmail.com>
2025-02-22 20:14:03 +0000

    Finalises the way that tagged links are displayed

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -3,5 +3,5 @@ from . import pages
 routes = [
     ("/", "front_page", pages.front_page, ["GET"]),
     ("/home", "home_page", pages.home, ["GET"]),
-    ("/links/tagged/<tag_name>", "post_by_tag", pages.tag, ["GET"]),
+    ("/links/tagged/<tag_name>", "links_by_tag", pages.tag, ["GET"]),
 ]
